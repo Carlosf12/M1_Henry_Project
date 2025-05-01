b2d9c76a2b4b2f52786810257ea2981a764ac192
Carlosf12 <carlosfigueira1212@gmail.com>
2025-05-01 13:45:20 -0400

    fix: Correct deleteActivity logic and complete createActivityCard function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,6 +16,22 @@ class Repository{
     }
 
     deleteActivity(id) {
-        this.activities = this.activities.filter(this.activities.filter(activity => activity.id !== id));
+        this.activities = this.activities.filter(activity => activity.id !== id);
     }
+}
+
+function createActivityCard(activity) {
+    const {id, title, description, imgUrl} = activity;
+
+    const card = document.createElement('div');
+    const titleElement = document.createElement('h3');
+    const descriptionElement = document.createElement('p');
+    const imageElement = document.createElement('img');
+
+    titleElement.innerHTML = title;
+    descriptionElement.innerHTML = description;
+    imageElement.src = imgUrl;
+    imageElement.alt = title;
+
+
 }
